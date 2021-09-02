7525814579a134f2c025319443901c5eda3bcca2
skunnavakkam <73597280+skunnavakkam@users.noreply.github.com>
2021-09-02 21:38:54 +0800

    Update board.py

--- src/board.py
+++ src/board.py
@@ -35,6 +35,7 @@ this makes it so that you can check piece color. x > 6 is black, x < 7 is white
 
 '''
 
+# stuff for generation
 N, S, E, W = 10, -10, -1, 1
 
 directions = {
@@ -45,6 +46,62 @@ directions = {
 }
 
 
+# stuff for evaluation
+piece = { 'P': 100, 'N': 280, 'B': 320, 'R': 479, 'Q': 929, 'K': 60000 }
+pst = {
+    'P': (   0,   0,   0,   0,   0,   0,   0,   0,
+            78,  83,  86,  73, 102,  82,  85,  90,
+             7,  29,  21,  44,  40,  31,  44,   7,
+           -17,  16,  -2,  15,  14,   0,  15, -13,
+           -26,   3,  10,   9,   6,   1,   0, -23,
+           -22,   9,   5, -11, -10,  -2,   3, -19,
+           -31,   8,  -7, -37, -36, -14,   3, -31,
+             0,   0,   0,   0,   0,   0,   0,   0),
+    'N': ( -66, -53, -75, -75, -10, -55, -58, -70,
+            -3,  -6, 100, -36,   4,  62,  -4, -14,
+            10,  67,   1,  74,  73,  27,  62,  -2,
+            24,  24,  45,  37,  33,  41,  25,  17,
+            -1,   5,  31,  21,  22,  35,   2,   0,
+           -18,  10,  13,  22,  18,  15,  11, -14,
+           -23, -15,   2,   0,   2,   0, -23, -20,
+           -74, -23, -26, -24, -19, -35, -22, -69),
+    'B': ( -59, -78, -82, -76, -23,-107, -37, -50,
+           -11,  20,  35, -42, -39,  31,   2, -22,
+            -9,  39, -32,  41,  52, -10,  28, -14,
+            25,  17,  20,  34,  26,  25,  15,  10,
+            13,  10,  17,  23,  17,  16,   0,   7,
+            14,  25,  24,  15,   8,  25,  20,  15,
+            19,  20,  11,   6,   7,   6,  20,  16,
+            -7,   2, -15, -12, -14, -15, -10, -10),
+    'R': (  35,  29,  33,   4,  37,  33,  56,  50,
+            55,  29,  56,  67,  55,  62,  34,  60,
+            19,  35,  28,  33,  45,  27,  25,  15,
+             0,   5,  16,  13,  18,  -4,  -9,  -6,
+           -28, -35, -16, -21, -13, -29, -46, -30,
+           -42, -28, -42, -25, -25, -35, -26, -46,
+           -53, -38, -31, -26, -29, -43, -44, -53,
+           -30, -24, -18,   5,  -2, -18, -31, -32),
+    'Q': (   6,   1,  -8,-104,  69,  24,  88,  26,
+            14,  32,  60, -10,  20,  76,  57,  24,
+            -2,  43,  32,  60,  72,  63,  43,   2,
+             1, -16,  22,  17,  25,  20, -13,  -6,
+           -14, -15,  -2,  -5,  -1, -10, -20, -22,
+           -30,  -6, -13, -11, -16, -11, -16, -27,
+           -36, -18,   0, -19, -15, -15, -21, -38,
+           -39, -30, -31, -13, -31, -36, -34, -42),
+    'K': (   4,  54,  47, -99, -99,  60,  83, -62,
+           -32,  10,  55,  56,  56,  55,  10,   3,
+           -62,  12, -57,  44, -67,  28,  37, -31,
+           -55,  50,  11,  -4, -19,  13,   0, -49,
+           -55, -43, -52, -28, -51, -47,  -8, -50,
+           -47, -42, -43, -79, -64, -32, -29, -32,
+            -4,   3, -14, -50, -57, -18,  13,   4,
+            17,  30,  -3, -14,   6,  -1,  40,  18),
+}
+
+doubled_pawn_penalty = -30
+isolated_pawn_penalty = -40
+tripled_or_greater_penalty = -70 
 
 class Position:
 
@@ -55,12 +112,22 @@ class Position:
     board = list()
     active_color = bool() # true if w 
     king = None
+    pawn_structure = [[0,0,0,0,0,0,0,0], [0,0,0,0,0,0,0,0]] # [white, black]. each digit maps to a column and counts the number of pawns in that column
+    score = int()
     
     # wc = white castle, bc = black castle, ep = enpassant, hm & fm = half and full move clocks
     def __init__(self, board, active_color, castling, ep, hm, fm):
 
         self.board, self.active_color, self.ep, self.hm, self.fm = board, active_color, ep, hm, fm  
         self.castling[0], self.castling[1], self.castling[2], self.castling[3] = 'K' in castling, 'Q' in castling, 'k' in castling, 'q' in castling
+
+        for square, piece in enumerate(board):
+            if piece == 1:
+                pawn_structure[0][(square % 10) - 1] += 1
+            elif piece == 7:
+                pawn_structure[1][(square % 10) - 1] += 1
+
+
 
     def is_square_attacked(self, square, color_to_check):
         
@@ -187,8 +254,16 @@ class Position:
         return pseudo_legal_moves
 
 
-    # move piece
-    def move(self, move): # the piece would be equal to the promotion value in the case of promotion
+    # use this instead, it creates a duplicate for searching
+    def move(self, move):
+        to_return = self
+
+        to_return._move(move)
+
+        return to_return
+
+    # don't use this to move piece
+    def _move(self, move): # the piece would be equal to the promotion value in the case of promotion
 
         start = move[0]
         end = move [1]
@@ -225,14 +300,15 @@ class Position:
             if not self.active_color:
                 self.fm += 1
         
-
+    def eval(self, move):
+        
         
     def __repr__(self) -> str:
         
         to_return = "\n"
 
         # white pieces look black on when changing between light and dark theme, but gonna assume coders use dark
-        dark_theme = [".", "♟︎", "♞", "♝", "♜", "♛", "♚", "♙", "♘", "♗", "♖", "♕", "♔"]
+        dark_theme = ["·", "♟︎", "♞", "♝", "♜", "♛", "♚", "♙", "♘", "♗", "♖", "♕", "♔"]
 
         piece_counter = 0
 
