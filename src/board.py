8ac3dcce1938f205ea2af3c82206cd9f45d75bb1
skunnavakkam <73597280+skunnavakkam@users.noreply.github.com>
2021-09-09 22:29:44 +0800

    Update board.py

--- src/board.py
+++ src/board.py
@@ -1,6 +1,34 @@
 # to see the source for this, look at https://en.wikipedia.org/wiki/0x88
 # Sudarshanagopal Kunnavakkam 8 September 2021
 # try no to steal my code please
+# i really reccomend you have rainbow brackets or smth similar installed
+
+'''
+    White   Black
+
+P   0001    1001
+N   0010    1010
+B   0011    1011
+R   0100    1100
+Q   0101    1101
+K   0110    1110
+
+empty = 0000
+
+X = Piece
+Y = Target
+
+capture:
+if (X ^ Y) > 1000
+
+empty:
+if (X ^ Y) == X
+
+own piece:
+if (X ^ Y) < 1000
+
+
+'''
 
 class Position:
 
@@ -31,11 +59,16 @@ class Position:
         self.hm = int(hm)
         self.ep = ep if ep != '-' else None
 
-        for square, piece in enumerate(pieces):
-            if square % 8 == 0:
+        piece_dict = {'P':1,'N':2,'B':3,'R':4,'Q':5,'K':6,'p':9,'n':10,'b':11,'r':12,'q':13,'k':14}
+
+        for row in pieces.split('/'):
+            for piece in row:
+                if piece.isdigit():
+                    self.board.append([0] * int(piece))
                 
-
-            if piece == '/':
-                continue
-
+                else:
+                    self.board.append(piece_dict[piece])
             
+            self.board.append([15] * 8)
+
+                
