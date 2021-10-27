f08a784952855e5583656def7216e88cd14244dd
skunnavakkam <73597280+skunnavakkam@users.noreply.github.com>
2021-10-28 07:16:27 +0800

    Update load_pgn.py

--- training/load_pgn.py
+++ training/load_pgn.py
@@ -11,55 +11,62 @@ so to detect the end of the game, you just have to detect one of these
 run this "C:\Program Files\pypy3.7-v7.3.5-win64\pypy3.exe" load_pgn.py
 '''
 
-from parse_data import *
-import re
-import time
+import chess
+import chess.pgn
+import io
+import random
 
+def parse_pgn(file):
 
-def parse_stuff(line, to_print):
-    try:
-        rating_white = int(re.findall(
-            'WhiteElo\s"([0-9]+)"', to_print)[0])
-        rating_black = int(re.findall(
-            'BlackElo\s"([0-9]+)"', to_print)[0])
+    with open(file) as f:
 
-        moves_list = re.findall(
-            '\s([a-zA-Z0-9=-]+)[+!?#]*\s', line)
-        result_str = re.findall('\s(\S*[0-9]-[0-9]\S*)', line)[0]
-
-        result_dict = {"1-0": 1, "0-1": 0, "1/2-1/2": 0.5}
-        result = result_dict[result_str]
-
-        position = find_positions(moves_list, result)
-
-        if position is not None:
-            with open('positions.txt', 'a') as f:
-                f.write("|".join(str(p) for p in position) + "\n")
-    except:
-        pass
-
-
-def load_pgn():
-    with open("raw-data/KingBase2019-A00-A39.pgn") as file:
+        # write heads for train, test, and cross-validation sets
+        # have to do something like this bc too much data to store in memory
+        f_train = open("training-data/train.txt", "a")
+        f_test = open("training-data/test.txt", "a")
+        f_cv = open("training-data/cv.txt", "a")
 
         to_print = ""
-        for line in file:
-            to_print += line  # o^2 but the variable won't get too large so it should be fine
-
-            if ("1/2-1/2" in line or "1-0" in line or "0-1" in line) and "Result" not in line:
-                # act on the pgn that has been stored
-
-                # tasks.append(
-                #     Process(target=parse_stuff, args=(line, to_print)))
-
-                parse_stuff(line, to_print)
-
+        for line in f:
+            to_print += line
+            if ("0-1" in line or "1-0" in line or "1/2-1/2" in line) and "Result" not in line:
+                
+                pgn = io.StringIO(to_print)
+                game = chess.pgn.read_game(pgn)
                 to_print = ""
+
+                positions = list(game.mainline())
+                choice = random.randrange(len(positions)-1)
+
+                position = positions[choice].board().fen()
+                next_move = str(positions[choice + 1].move)
+
+                to_write = "|".join((position, next_move, game.headers["Result"])) + "\n"
+
+                writer = random.randint(1,11) # generating between 1 and 11 inclusive
+
+                if writer <= 6:
+                    f_train.write(to_write)
+                elif writer <= 8:
+                    f_cv.write(to_write)
+                else:
+                    f_test.write(to_write)
+        
+        f_train.close()
+        f_test.close()
+        f_cv.close()
+                
+                
+
+
+
+
+
 
 
 def main():
 
-    load_pgn()
+    parse_pgn("raw-data/KingBase2019-A00-A39.pgn")
 
 
 if __name__ == '__main__':
