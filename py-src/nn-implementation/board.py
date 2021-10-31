1dd869530f483b020ba7f67f09daeb08bed23070
skunnavakkam <73597280+skunnavakkam@users.noreply.github.com>
2021-11-01 07:18:45 +0800

    added SmartBoard

---
+++ py-src/nn-implementation/board.py
@@ -0,0 +1,15 @@
+
+import chess
+
+Bitboard = int
+Square = int
+
+class SmartBoard(chess.Board):
+
+    def image(self) -> []:
+
+    def weak_squares(self, color) -> Bitboard:
+        pass
+
+    def weak_complex(self, color) -> Bitboard:
+        pass
